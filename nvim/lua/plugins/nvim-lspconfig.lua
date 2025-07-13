4d9aaae64ff519ef909bad764bd0d8b09b60122a
Henrik Romnes <hbr@synergysky.com>
2025-07-13 13:03:03 +0200

    lsp keybinds

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -38,6 +38,14 @@ return {
                 vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, {})
             end
 
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+            vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+            vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, {})
+
             require("lspconfig").lua_ls.setup {
                 on_attach = on_attach
             }
