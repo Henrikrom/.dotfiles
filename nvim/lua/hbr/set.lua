570681616531bc90309f1833eccf5eb15e8c7768
Henrik Romnes <hbr@synergysky.com>
2025-07-13 13:01:11 +0200

    nvim snippets + small config changes

--- nvim/lua/hbr/set.lua
+++ nvim/lua/hbr/set.lua
@@ -23,7 +23,7 @@ vim.opt.ignorecase = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "no"
+vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 --vim.opt.colorcolumn = "80"
