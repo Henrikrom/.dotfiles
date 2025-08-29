c5526ad295ce501fc04791c791f95dd41dd7c5b3
Henrik Romnes <hbr@synergysky.com>
2025-08-29 15:00:12 +0200

    vim test use dotnet test instead of dotnet xunit

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -1,12 +1,19 @@
 return {
-    "vim-test/vim-test",
-    dependencies = {
-        "preservim/vimux"
-    },
-    vim.keymap.set('n', '<leader>bf', ':TestNearest<CR>'),
-    vim.keymap.set('n', '<leader>B', ':TestFile<CR>'),
-    vim.keymap.set('n', '<leader>bs', ':testSuite<CR>'),
-    vim.keymap.set('n', '<leader>bl', ':TestLast<CR>'),
-    vim.keymap.set('n', '<leader>bv', ':TestVisit<CR>'),
+  "vim-test/vim-test",
+  dependencies = {
+    "preservim/vimux"
+  },
+  config = function()
+    -- Keymaps
+    vim.keymap.set('n', '<leader>bf', ':TestNearest<CR>')
+    vim.keymap.set('n', '<leader>B', ':TestFile<CR>')
+    vim.keymap.set('n', '<leader>bs', ':TestSuite<CR>')
+    vim.keymap.set('n', '<leader>bl', ':TestLast<CR>')
+    vim.keymap.set('n', '<leader>bv', ':TestVisit<CR>')
+
+    -- Strategy: run inside vimux
     vim.cmd("let test#strategy = 'vimux'")
+
+    vim.cmd("let g:test#csharp#runner = 'dotnettest'")
+  end
 }
