6781ad50dcaa41fb8bfbe5add47e82b0c45aec53
Henrik Romnes <hbr@synergysky.com>
2025-10-22 21:41:59 +0200

    zenbones theme

---
+++ nvim/lua/plugins/zenbones.lua
@@ -0,0 +1,14 @@
+return {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.o.background = "dark"
+        vim.g.zenbones = {
+            darkness = "stark",        -- options: 'warm', 'stark', 'dim', 'bright', 'default'
+        }
+
+        vim.cmd.colorscheme("zenbones")
+    end,
+}
