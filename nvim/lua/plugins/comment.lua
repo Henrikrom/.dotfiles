e9a42f8d2e2cf681a867eb2fdd9e02df68c5b614
Henrik Romnes <hbr@synergysky.com>
2025-10-23 10:16:43 +0200

    comment lua for razor

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,8 +1,22 @@
 return {
     "numToStr/Comment.nvim",
     name = "Comment.nvim",
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+    },
     config = function()
-        require("Comment").setup()
+        require("Comment").setup({
+            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "razor",
+            callback = function()
+                if vim.bo.commentstring == "" or vim.bo.commentstring == nil then
+                    vim.bo.commentstring = "<!-- %s -->"
+                end
+            end,
+        })
     end,
 }
 
